b46e12cfc198a12721c08e6f2b2899c6d64f3435
t.kranzer <t.kranzer@gmail.com>
2015-01-16 07:37:32 +0100

    labbus

---
+++ Wasser-App/src/at/fhhgb/mc/wasserapp/labbus/LabbusActivity.java
@@ -0,0 +1,19 @@
+package at.fhhgb.mc.wasserapp.labbus;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import at.fhhgb.mc.wasserapp.R;
+import at.fhhgb.mc.wasserapp.R.layout;
+
+public class LabbusActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_labbus);
+		
+		
+	}
+}
